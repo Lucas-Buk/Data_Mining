6ffd822ead927586e02ed5317656d5f37f06ca2f
Lucas-Buk <lucasbukcardoso@gmail.com>
2021-04-20 00:42:31 -0300

    Finalização do projeto

--- Titanic/titanic/README.md
+++ Titanic/titanic/README.md
@@ -1,4 +1,4 @@
-# Titanic
+# titanic
 
 ## Overview
 
--- Titanic/titanic/src/titanic/__main__.py
+++ Titanic/titanic/src/titanic/__main__.py
@@ -25,7 +25,7 @@
 #
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Titanic file for ensuring the package is executable
+"""titanic file for ensuring the package is executable
 as `titanic` and `python -m titanic`
 """
 from pathlib import Path
